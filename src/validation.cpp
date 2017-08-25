ac6d168f3345e619e02da5cd9174c2c347105170
paolomazzocchi <mazzocchip@live.it>
2017-08-25 10:45:05 +0200

    set coinbase reward to 1 and created getBlockReward

--- src/validation.cpp
+++ src/validation.cpp
@@ -1165,14 +1165,24 @@ bool ReadBlockFromDisk(CBlock& block, const CBlockIndex* pindex, const Consensus
 
 CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
 {
-    int halvings = nHeight / consensusParams.nSubsidyHalvingInterval;
+    //int halvings = nHeight / consensusParams.nSubsidyHalvingInterval;
     // Force block reward to zero when right shift is undefined.
-    if (halvings >= 64)
-        return 0;
+    //if (halvings >= 64)
+    //    return 0;
 
-    CAmount nSubsidy = 50 * COIN;
+    //CAmount nSubsidy = 50 * COIN;
+    CAmount nSubsidy = 1 * COIN;
+
     // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
-    nSubsidy >>= halvings;
+    //nSubsidy >>= halvings;
+	
+    return nSubsidy;
+}
+
+CAmount GetBlockReward(int amount)
+{
+    CAmount nSubsidy = amount * COIN;
+
     return nSubsidy;
 }
 
