6abb645427794c68be22e7a2b984c6e12ed70e5d
paolomazzocchi <mazzocchip@live.it>
2017-08-25 16:37:47 +0200

    removed coinbase amount check

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -179,7 +179,7 @@ UniValue generate(const JSONRPCRequest& request)
     if (coinbaseScript->reserveScript.empty())
         throw JSONRPCError(RPC_INTERNAL_ERROR, "No coinbase script available (mining requires a wallet)");
 
-    return generateBlocks(coinbaseScript, nGenerate, nMaxTries, true, 2);
+    return generateBlocks(coinbaseScript, nGenerate, nMaxTries, true, 1);
 }
 
 UniValue generatetoaddress(const JSONRPCRequest& request)
