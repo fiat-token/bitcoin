b6ab4ccdd46df52b3d2fc75d9ade584b3e846f9a
CalogeroMandracchia <kalos9214@gmail.com>
2017-09-01 16:37:38 +0200

    test output coin value

--- src/amount.h
+++ src/amount.h
@@ -12,11 +12,13 @@
 #include <string>
 
 #include "util.h" // for initializing "COIN" from bitcoin.conf
+#include <iostream>
 
 /** Amount in satoshis (Can be negative) */
 typedef int64_t CAmount;
 
 static const CAmount COIN = GetArg("-coin", 100000000);
+std::cout << "coin modificato: " << COIN << "-";
 static const CAmount FIATTOKEN = 10000;
 static const CAmount CENT = 1000000;
 
